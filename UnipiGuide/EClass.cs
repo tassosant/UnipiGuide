0eaad548820024aee1fcdb81eeb5b53e1cc42576
evangelia th <e.thermou@outlook.com>
2023-09-12 23:15:55 +0300

    calendar functionality and textboxes in eclass page

--- UnipiGuide/EClass.cs
+++ UnipiGuide/EClass.cs
@@ -15,9 +15,28 @@ namespace UnipiGuide
         public EClass()
         {
             InitializeComponent();
+            this.monthCalendar1.BoldedDates = new System.DateTime[] { new System.DateTime(2023, 9, 18, 0, 0, 0, 0) };
         }
 
         private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            this.textBox1.Text =
+              monthCalendar1.SelectionRange.Start.Date.ToShortDateString();          
+        }
+
+        private void button_WOC1_Click(object sender, EventArgs e)
+        {
+            this.textBox2.Text = "Δεν έχεις πολύ χρόνο!";
+              
+        }
+
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
